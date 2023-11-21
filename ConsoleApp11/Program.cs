f77f725439722c86789caf7a7bfc6bb96dd9fcf1
MJOghbatalab <m.j.oghbatalab@gmail.com>
2023-11-21 13:56:20 +0330

    .

--- ConsoleApp11/Program.cs
+++ ConsoleApp11/Program.cs
@@ -1,4 +1,3 @@
 ﻿Console.WriteLine("I want to test Sync");
 Console.WriteLine("I want to test Sync by user 1");
-Console.WriteLine("I want to test Sync by user 1 on master branch");
-
+Console.WriteLine("I want to test Sync by user 1 on Branch1");
